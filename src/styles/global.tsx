6aae209ae2318928f691b2a6922a50ccc278ea99
vicentecarlos <soniamariadias2012@hotmail.com>
2023-11-18 15:18:00 -0300

    feat: implements global-style

---
+++ src/styles/global.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Montserrat', sans-serif;
+        background-color: #141c2f;
+        color: white;
+    }
+`;
+
+export { GlobalStyle };
